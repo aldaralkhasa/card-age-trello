af22ce18a5415f6fd90b6b61ff649bdca29d444d
Malick <malick.s.mohamed@gmail.com>
2021-01-13 10:19:05 +0300

    console log card info

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,16 +1,19 @@
-let counter = 0;
-
 window.TrelloPowerUp.initialize({
   "card-badges": function (t, opts) {
     let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
+
+    console.log(t);
+    console.log(opts);
+
     return t
       .card("name")
       .get("name")
       .then(function (cardName) {
-        console.log("We just loaded the card name for fun: " + cardName);
         return [
           {
             dynamic: function () {
+              let counter = 0;
+
               return {
                 text: "Dynamic counter: " + counter++,
                 color: "green",
