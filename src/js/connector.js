5c36f32ac429c87d272a2cd07845c971c1afaf02
Malick <malick.s.mohamed@gmail.com>
2021-01-13 10:50:16 +0300

    console log board data

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,8 +2,8 @@ window.TrelloPowerUp.initialize({
   "card-badges": function (t, opts) {
     let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
 
-    return t.card("all").then(function (card) {
-      console.log(JSON.stringify(card, null, 2));
+    return t.board("all").then(function (bord) {
+      console.log(JSON.stringify(bord, null, 2));
 
       return [
         {
