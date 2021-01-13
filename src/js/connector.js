d5677c89af52476bf0a284567960196bb389ee1b
Malick <malick.s.mohamed@gmail.com>
2021-01-13 11:30:17 +0300

    test last date activity

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,15 +2,23 @@ window.TrelloPowerUp.initialize({
   "card-badges": function (t, opts) {
     let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
 
-    return t.card("all").then(function (card) {
-      console.log(JSON.stringify(card, null, 2));
+    return t.card("all").then(function ({dateLastActivity}) {
+        
 
       let counter = 0;
       return [
         {
-          dynamic: function () {
+          dynamic: function (dateLastActivity) {
+            const date1 = new Date(dateLastActivity);
+            const date2 = new Date();
+          
+            const diffTime = Math.abs(date2 - date1);
+            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+          
+            // console.log(diffTime + " milliseconds");
+            // console.log(diffDays + " days");
             return {
-              text: "time: " + counter++,
+              text: diffDays + " days",
               color: "blue",
               refresh: 10, // in seconds
             };
