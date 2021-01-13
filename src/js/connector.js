3a735e5e215457274ccbc23a8b59dff369df2d1b
Malick <malick.s.mohamed@gmail.com>
2021-01-13 10:09:46 +0300

    create a counter badge

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,28 +1,26 @@
+let counter = 0;
+
 window.TrelloPowerUp.initialize({
-    "card-badges": function (t, opts) {
-      let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
-      return t
-        .card("name")
-        .get("name")
-        .then(function (cardName) {
-          console.log("We just loaded the card name for fun: " + cardName);
-          return [
-            {
-              dynamic: function () {
-                return {
-                  text: "Dynamic " + (Math.random() * 100).toFixed(0).toString(),
-                  color: "green",
-                  refresh: 10, // in seconds
-                };
-              },
+  "card-badges": function (t, opts) {
+    let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
+    return t
+      .card("name")
+      .get("name")
+      .then(function (cardName) {
+        console.log("We just loaded the card name for fun: " + cardName);
+        return [
+          {
+            dynamic: function () {
+              return {
+                text: "Dynamic counter: " + counter++,
+                color: "green",
+                refresh: 10, // in seconds
+              };
             },
-            {
-              text: "Static",
-              color: "blue",
-            },
-          ];
-        });
-    },
+          },
+        ];
+      });
+  },
 });
 
 // for card front badges only
