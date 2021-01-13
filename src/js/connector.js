4eee2c1ffd197407bac78adbd9f2c9f34962adab
Malick <malick.s.mohamed@gmail.com>
2021-01-13 10:33:45 +0300

    console log all data

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,27 +2,23 @@ window.TrelloPowerUp.initialize({
   "card-badges": function (t, opts) {
     let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
 
-    console.log(t);
-    console.log(opts);
+    return t.getAll().then(function (data) {
+      console.log(data);
 
-    return t
-      .card("name")
-      .get("name")
-      .then(function (cardName) {
-        return [
-          {
-            dynamic: function () {
-              let counter = 0;
+      return [
+        {
+          dynamic: function () {
+            let counter = 0;
 
-              return {
-                text: "Dynamic counter: " + counter++,
-                color: "green",
-                refresh: 10, // in seconds
-              };
-            },
+            return {
+              text: "Dynamic counter: " + counter++,
+              color: "green",
+              refresh: 10, // in seconds
+            };
           },
-        ];
-      });
+        },
+      ];
+    });
   },
 });
 
