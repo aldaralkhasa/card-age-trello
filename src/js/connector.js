5eff7ef1bc672d7543ab1fe70f8d041a5063a161
Malick <malick.s.mohamed@gmail.com>
2021-01-18 08:48:39 +0300

    adjust function parameter to accept card as an object

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,26 +1,23 @@
 window.TrelloPowerUp.initialize({
   "card-badges": function (t, opts) {
-    let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
-
-    return t.card("all").then(function ({dateLastActivity}) {
-        
-
+    return t.card("all").then(function (card) {
+      let { dateLastActivity } = card;
       let counter = 0;
       return [
         {
           dynamic: function (dateLastActivity) {
             const date1 = new Date(dateLastActivity);
             const date2 = new Date();
-          
+
             const diffTime = Math.abs(date2 - date1);
             const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-          
-            // console.log(diffTime + " milliseconds");
-            // console.log(diffDays + " days");
+
+            console.log(diffTime + " milliseconds");
+            console.log(diffDays + " days");
             return {
               text: diffDays + " days",
               color: "blue",
-              refresh: 10, // in seconds
+              refresh: 1110, // in seconds
             };
           },
         },
